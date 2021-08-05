ce7db49f606477dd9786a6532260231995afff8d
u-kaji <kurinodechawonomou@gmail.com>
2021-08-05 19:26:00 +0900

    新規登録画面、ログイン画面アクセス

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true
+  validates :name_first, presence: true
+  validates :name_last, presence: true
+  validates :name_first_kana, presence: true
+  validates :name_last_kana, presence: true
+  validates :birthday, presence: true
 end
