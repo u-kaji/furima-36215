d8362117378058f4da3c12053e1b680184bb8871
u-kaji <kurinodechawonomou@gmail.com>
2021-08-06 14:04:11 +0900

    新規登録・ログイン画面修正

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,9 @@
 class UserController < ApplicationController
   def new
-    
+  end
+
+  def create
+    @user = User.new
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items, only: [:index]
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :destroy]
 end
