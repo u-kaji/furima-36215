922dc7c6f74caf55119c569f2201e96711c773be
u-kaji <kurinodechawonomou@gmail.com>
2021-08-08 17:20:43 +0900

    ログイン状態で出品ページへの変遷変化

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items, only: [:index]
+  resources :items, only: [:index, :edit, :new]
   resources :users, only: [:new, :create, :destroy]
 end
