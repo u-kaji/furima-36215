0b1f559faa5fdde0db696abdb08cdcc3b824503a
u-kaji <kurinodechawonomou@gmail.com>
2021-08-10 12:38:31 +0900

    出品投稿、エラー確認

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,23 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 end
+
+
+
+private
+
+def item_params
+  params.require(:item).permit(:name, :detail, :category_id, :condition_id, :send_cost_id, :prefecture_id, :send_day_id, :price, :user_id, :image).merge(user_id: current_user.id)
+end
+
+
--- app/models/item.rb
+++ app/models/item.rb
@@ -14,6 +14,8 @@ class Item < ApplicationRecord
     validates :price, format:{with: /\A[0-9]+\z/}, inclusion:{in: 300..9_999_999}
   end
 
+  has_one_attached :image
+  
     extend ActiveHash::Associations::ActiveRecordExtensions
     belongs_to :category
     belongs_to :condition
