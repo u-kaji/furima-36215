ac197c061dfd4a6b31d040ff8dee386d84919c9b
u-kaji <kurinodechawonomou@gmail.com>
2021-08-19 12:44:14 +0900

    指摘箇所４

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,9 +24,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if @item.buy.present?
-      redirect_to root_path
-    end
   end
 
   def update
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../card")
-//require("../price_cost_benefit");
+require("../price_cost_benefit");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
